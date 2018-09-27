499b550ff3cdf2136152f802e49f1684600efcc2
Naturalizer <joesm.genie@gmail.com>
2018-09-28 00:53:50 +0700

    Update Entity

---
+++ StockServer/src/main/java/com/stock/StockDB/Entity/OrderProduct.java
@@ -0,0 +1,30 @@
+package com.stock.StockDB.Entity;
+
+import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+@Entity
+@Getter @Setter
+@Table(name="OrderProduct")
+public class OrderProduct {
+    @Id
+    @GeneratedValue
+    private @NonNull long OP_Id;
+    @Column(length = 100)
+    private int OP_OrderAmount;
+    private double OP_Total_Price;
+
+    
+    protected OrderProduct(){}
+    public OrderProduct(int P_OrderAmount, double OP_Total_Price){
+        this.OP_OrderAmount = P_OrderAmount;
+        this.OP_Total_Price = OP_Total_Price;
+    }
+}
---
+++ StockServer/src/main/java/com/stock/StockDB/Entity/Stock.java
@@ -0,0 +1,47 @@
+package com.stock.StockDB.Entity;
+
+import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+@Entity
+@Getter @Setter
+@Table(name="Stock")
+public class Stock{
+
+    @Id
+    @GeneratedValue
+    private @NonNull long S_Id;
+    @Column(length = 100)
+    private int P_Amount;
+
+    // Stock --> Product
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name= "P_Id")
+    @JsonBackReference
+    private Product product;
+    
+    // Stock --> Warehouse
+    @ManyToOne(fetch = FetchType.LAZY)   
+    @JoinColumn(name= "WH_Id")     
+    @JsonBackReference 
+    private Warehouse warehouse;    
+
+    // Stock --> OrderProduct
+    @OneToOne
+    @JoinColumn(name = "OP_Id")
+    private OrderProduct orderProduct;
+
+    protected Stock(){}
+    public Stock(int P_Amount){
+        this.P_Amount = P_Amount;
+    }
+
+}
