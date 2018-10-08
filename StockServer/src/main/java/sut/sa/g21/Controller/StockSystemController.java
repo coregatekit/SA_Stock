6e11b92f3c3c7ae80aba459184cac9c26d78192c
Naturalizer <joesm.genie@gmail.com>
2018-10-08 23:11:52 +0700

    Add POST Product
    เพิ่มคำสั่งสำหรับการเพิ่ม Product ใหม่

--- StockServer/src/main/java/sut/sa/g21/Controller/StockSystemController.java
+++ StockServer/src/main/java/sut/sa/g21/Controller/StockSystemController.java
@@ -1,13 +1,21 @@
 package sut.sa.g21.Controller;
 
+import java.io.IOException;
+import java.net.URLDecoder;
 import java.util.Collection;
 import java.util.Optional;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import sut.sa.g21.Entity.*;
@@ -26,20 +34,14 @@ public class StockSystemController {
     public Collection<OrderProduct> orderProducts(){
         return orderProductRepository.findAll();
     }
-    @GetMapping("/Product/{orderproductId}")
-    public Optional<OrderProduct> takeinOrderProductByid(@PathVariable Long orderproductId) {
-        return orderProductRepository.findById(orderproductId);
-    }
+    
 
     // Stock
     @GetMapping("/Stock")
     public Collection<Stock> stocks() {
         return stockRepository.findAll();
     }
-    @GetMapping("/Stock/{stockId}")
-    public Optional<Stock> takeinStockByid(@PathVariable Long stockId) {
-        return stockRepository.findById(stockId);
-    }
+    
 
     // Product
     @GetMapping("/Product")
@@ -50,14 +52,37 @@ public class StockSystemController {
     public Optional<Product> takeinProductByid(@PathVariable Long productId ) {
         return productRepository.findById(productId);
     }
+    /*
+    @PostMapping("/AddProduct")
+    public void addProduct(@RequestBody String dataProduct) throws JsonParseException, IOException {
+        final String decoded = URLDecoder.decode(dataProduct, "UTF-8");
+        dataProduct = decoded;
+        Product newProduct = new Product();
+        if (dataProduct.charAt(0) == '{') {
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode actualObj = mapper.readTree(dataProduct);
+
+            JsonNode jsonNode = actualObj.get("productName");
+            newProduct.setProductName(jsonNode.textValue());
+            jsonNode = actualObj.get("productDetail");
+            newProduct.setProductDetail(jsonNode.textValue());
+            jsonNode = actualObj.get("productImgUrl");
+            newProduct.setProductImgUrl(jsonNode.textValue());
+            jsonNode = actualObj.get("productProce");
+            newProduct.setProductPrice(jsonNode.doubleValue());
+            productRepository.save(newProduct);
+        }
+    }
+    */
+    // ทดสอบโดย ใช้คำสั่ง curl -iX POST http://localhost:8080/Product/addProduct/AWM/AWM/AWM/5900
     @PostMapping("/Product/addProduct/{productName}/{productDetail}/{productImgUrl}/{productPrice}")
     public Product addProduct(@PathVariable String productName, @PathVariable String productDetail, @PathVariable String productImgUrl, @PathVariable double productPrice) {
-        Product newProduct = new Product();
-        newProduct.setProductName(productName);
-        newProduct.setProductDetail(productDetail);
-        newProduct.setProductImgUrl(productImgUrl);
-        newProduct.setProductPrice(productPrice);
-        return productRepository.save(newProduct);
+       Product newProduct = new Product();
+       newProduct.setProductName(productName);
+       newProduct.setProductDetail(productDetail);
+       newProduct.setProductImgUrl(productImgUrl);
+       newProduct.setProductPrice(productPrice);
+       return productRepository.save(newProduct);
     }
 
     // Warehouse
